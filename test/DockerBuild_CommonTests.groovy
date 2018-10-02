89ee4aefd1d22151d8aed68aaa270fe859e6ac85
Pavel Bakulev <p.bakulev@bilderlings.eu>
2018-10-02 11:47:35 +0300

    Added tests for DockerBuild with null params

--- test/DockerBuild_CommonTests.groovy
+++ test/DockerBuild_CommonTests.groovy
@@ -37,4 +37,16 @@ class DockerBuild_CommonTests extends GroovyTestCase {
 
     }
 
+    @Test
+    void test_DockerBuild_NullParameter_DefaultParameters(){
+        def actualCommands = []
+        dockerBuild_.sh = { command -> actualCommands << command; return null}
+        def expectedCommands = ['docker build . -t registry.com/bilderlings/Job_Name:master-1 -f ./Dockerfile']
+
+        dockerBuild_ null
+
+        assertEquals(expectedCommands, actualCommands)
+
+    }
+
 }
