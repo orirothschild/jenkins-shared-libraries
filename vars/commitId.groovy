749359489715c419a0a2e1746ff63f1fde5baeba
Pavel Bakulev <p.bakulev@bilderlings.eu>
2018-12-04 09:28:40 +0200

    Updated commitId, check variable COMMIT_ID

--- vars/commitId.groovy
+++ vars/commitId.groovy
@@ -1,4 +1,15 @@
 
 def call(){
-    sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%H'")
+    def gitCommit = env.GIT_COMMIT?.ToString()?.trim()
+    if (gitCommit != null && gitCommit != ''){
+        return gitCommit
+    }
+    try{
+        return sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%H'")
+    } catch(Exception ex) {
+        if (ex.message.contains('git: not found')){
+            return sh(returnStdout: true, script: "cat ${env.WORKSPACE}/.git/HEAD | awk '{print \$2}' | xargs -I {} cat ${env.WORKSPACE}/.git/{}")
+        }
+        throw ex
+    }
 }
