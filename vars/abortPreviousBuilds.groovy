5ceb1e134723ea90110801cd37c8bfe6449ad9f5
Pavel Bakulev <p.bakulev@bilderlings.eu>
2019-04-12 11:17:00 +0300

    Added abortPreviousBuilds

---
+++ vars/abortPreviousBuilds.groovy
@@ -0,0 +1,12 @@
+
+def call() {
+	def jobName = env.JOB_NAME
+	def buildNumber = env.BUILD_NUMBER.toInteger()
+	def currentJob = Jenkins.instance.getItemByFullName(jobName)
+
+	for (def build : currentJob.builds) {
+		if (build.isBuilding() && build.number.toInteger() < buildNumber) {
+			build.doStop()
+		}
+	}
+}
