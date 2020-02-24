7bd7ff20e7cae13baa4e83efffa90146330b1009
Pavel <9974560+pbakulev@users.noreply.github.com>
2020-02-24 13:27:33 +0200

    Fix gitlab status url (#12)
    * Added support for ssh and https gitlab url in gitlabStatus()

--- test/TestData/ShellTestData.groovy
+++ test/TestData/ShellTestData.groovy
@@ -1,13 +1,27 @@
 package TestData
 
-static Closure getShellClosure(){
+static Closure getShellClosureSSH(){
     return { Map map ->
         if (map.returnStdout) {
             if (map.script == "git log -n 1 --pretty=format:'%H'"){
                 return "1111111222222222222222222222222222222222"
             }
             if (map.script == "git config remote.origin.url"){
-                return "git@github.com:bilderlings/jenkins-shared-libraries.git"
+                return "git@gitlab.com:company/project/repository.git"
+            }
+        }
+        throw new Exception("Undefined shell parameters: ${map}")
+    }
+}
+
+static Closure getShellClosureHTTPS(){
+    return { Map map ->
+        if (map.returnStdout) {
+            if (map.script == "git log -n 1 --pretty=format:'%H'"){
+                return "1111111222222222222222222222222222222222"
+            }
+            if (map.script == "git config remote.origin.url"){
+                return "https://gitlab.com/company/project/repository.git"
             }
         }
         throw new Exception("Undefined shell parameters: ${map}")
